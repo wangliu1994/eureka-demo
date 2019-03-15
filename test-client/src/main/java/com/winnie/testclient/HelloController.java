cf40a8f5355b9749e2361cdb128b76367c658f7e
winnie <wangliu023@qq.com>
2019-03-15 16:42:01 +0800

    sync

--- test-client/src/main/java/com/winnie/testclient/HelloController.java
+++ test-client/src/main/java/com/winnie/testclient/HelloController.java
@@ -1,4 +1,4 @@
-package com.winnie.testserver;
+package com.winnie.testclient;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
--- test-client/src/main/java/com/winnie/testclient/TestClientApplication.java
+++ test-client/src/main/java/com/winnie/testclient/TestClientApplication.java
@@ -1,17 +1,15 @@
-package com.winnie.testserver;
+package com.winnie.testclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //注意：早期的版本（Dalston及更早版本）还需在启动类上添加注解@EnableDiscoveryClient 或@EnableEurekaClient ，从Edgware开始，该注解可省略。
 //@EnableDiscoveryClient
 @SpringBootApplication
-public class TestServerApplication {
+public class TestClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestServerApplication.class, args);
+        SpringApplication.run(TestClientApplication.class, args);
     }
 
 }
